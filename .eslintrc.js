6c61dc5f1b3e8c37f5cfeacfea42b335ad948053
Alex Chiu <alexchiu11@gmail.com>
2020-09-11 13:03:21 +0100

    Linted 🧹

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+  module.exports = {
+    root: true,
+    env: {
+      node: true
+    },
+    extends: [
+      "plugin:vue/vue3-essential",
+      "plugin:vue/vue3-recommended",
+      "eslint:recommended",
+      "@vue/typescript"
+    ],
+    parserOptions: {
+      "parser": "@typescript-eslint/parser"
+    },
+    rules: {
+       "quotes": ["warn", "double"]
+    }
+  }
