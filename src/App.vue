0ffc60e94a30f3d515dc13a166427a7de44051b3
Alex Chiu <alexchiu11@gmail.com>
2020-09-11 21:13:19 +0100

    Removed styles from app component

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,4 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  height: 100vh;
-  background-color: antiquewhite;
-}
 </style>
