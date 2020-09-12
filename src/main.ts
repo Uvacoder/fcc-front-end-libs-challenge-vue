f916bae1655300c9c1b2c463bdce196e5fb92dc9
Alex Chiu <alexchiu11@gmail.com>
2020-09-12 14:03:32 +0100

    Hack to get vue dev tools to work for vue 3

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 import "./assets/main.css"
+import { devtools } from '@/devtools';
 
-createApp(App).use(router).mount("#app")
+createApp(App)
+.use(router)
+.use(devtools)
+.mount("#app")
