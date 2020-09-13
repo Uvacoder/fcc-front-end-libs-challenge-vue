070375a60286ea43d36e8baacf4cda832ad9042a
Alex Chiu <alexchiu11@gmail.com>
2020-09-13 12:52:06 +0100

    Disabled dev tools hack

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 import "./assets/main.css"
-import { devtools } from '@/devtools';
+// import { devtools } from '@/devtools';
 
 createApp(App)
 .use(router)
-.use(devtools)
+// .use(devtools)
 .mount("#app")
