d00067a045a43fd5167ab70be8420938cf55340f
Alex Chiu <alexchiu11@gmail.com>
2020-09-13 16:04:19 +0100

    Removed devtools hack

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 import "./assets/main.css"
-// import { devtools } from '@/devtools';
 
 createApp(App)
 .use(router)
-// .use(devtools)
 .mount("#app")
