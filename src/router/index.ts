56052377c1770f426d840a41139b00d2e16fd838
Alex Chiu <alexchiu11@gmail.com>
2020-09-17 20:22:12 +0100

    Fixed route issue

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,7 +25,7 @@ const routes: Array<RouteRecordRaw> = [
   },
   {
     path: "/random-quote-machine",
-    name: "Pomodoro",
+    name: "RandomQuoteMachine",
     component: (): Component => import("../views/RandomQuoteMachine.vue"),
   },
 ];
