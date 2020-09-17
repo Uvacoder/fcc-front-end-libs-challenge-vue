9dc5accc14b6915cccbddca50bc4cfc1c3bf34eb
Alex Chiu <alexchiu11@gmail.com>
2020-09-17 20:10:14 +0100

    Inital implementation of Drum machine

---
+++ src/components/DumPad.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="bg-red-600 p-5 m-5 h-16" @click="play">
+    <audio ref="chord" preload :src="audio"></audio>
+    {{ name }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "DrumPad",
+  props: {
+    audio: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    const chord = ref<HTMLAudioElement>();
+
+    function play(): void {
+      chord?.value?.play();
+    }
+
+    return { play, chord };
+  },
+});
+</script>
+
+<style scoped></style>
--- src/views/DrumMachine.vue
+++ src/views/DrumMachine.vue
@@ -1,71 +1,56 @@
 <template>
-  <div class="text-center">Drum Machine</div>
-
-  <button
-    class="bg-red-600 p-5 m-5 h-16 w-3 flex justify-center"
-    @click="handleKey"
-  >
-    <audio
-      ref="chord"
-      preload
-      src="https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
-    ></audio>
-    chord
-  </button>
+  <div id="drum-machine-view" class="flex flex-col justify-center h-screen">
+    <div id="drumpad-container" class="grid grid-flow-row grid-cols-3">
+      <DrumPad
+        name="Chord 1"
+        audio="https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
+      />
+      <DrumPad
+        name="Chord 2"
+        audio="https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
+      />
+      <DrumPad
+        name="Chord 3"
+        audio="https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
+      />
+      <DrumPad
+        name="Sample 1"
+        audio="https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
+      />
+      <DrumPad
+        name="Sample 2"
+        audio="https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"
+      />
+      <DrumPad
+        name="Sample 3"
+        audio="https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"
+      />
+      <DrumPad
+        name="Kick 1"
+        audio="https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
+      />
+      <DrumPad
+        name="Kick 2"
+        audio="https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
+      />
+      <DrumPad
+        name="Snare"
+        audio="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
-const keyPressLookup = {
-  Q: {
-    name: "Chord-1",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
-  },
-  W: {
-    name: "Chord-2",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
-  },
-  E: {
-    name: "Chord-3",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
-  },
-  A: {
-    name: "Shaker",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
-  },
-  S: {
-    name: "Open-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
-  },
-  D: {
-    name: "Closed-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
-  },
-  Z: {
-    name: "Punchy-Kick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
-  },
-  X: {
-    name: "Side-Stick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
-  },
-  C: {
-    name: "Snare",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
-  },
-};
+import { defineComponent } from "vue";
+import DrumPad from "@/components/DumPad.vue";
 
 export default defineComponent({
+  components: {
+    DrumPad,
+  },
   setup() {
-    console.log(keyPressLookup);
-    const chord = ref<any>(null);
-
-    function handleKey(): void {
-      console.log(chord.value);
-      chord.value.play();
-    }
-
-    return { handleKey, chord };
+    return {};
   },
 });
 </script>
